a1cf405358be0168a8887824bec0d01d2672135f
Nikolai Bock <nikolai.bock@geoinform.fh-mainz.de>
2015-02-26 17:05:15 +0100

    Test loggt jetzt mit Logger und keine SysOuts mehr

--- spring-sos/src/test/java/de/i3mainz/springframework/swe/sos/SOSTest.java
+++ spring-sos/src/test/java/de/i3mainz/springframework/swe/sos/SOSTest.java
@@ -6,22 +6,26 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.sun.org.apache.bcel.internal.generic.ISTORE;
 
 import de.i3mainz.springframework.swe.n52.sos.model.FeatureOfInterest;
 import de.i3mainz.springframework.swe.n52.sos.model.Observation;
 import de.i3mainz.springframework.swe.n52.sos.model.Sensor;
 
-@SuppressWarnings("deprecation")
 @ContextConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 public class SOSTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SOSTest.class);
 
     @Autowired
     public SOSService service;
@@ -44,44 +48,42 @@ public class SOSTest {
     @Test
     public void testSOS() throws Exception {
         Assert.assertTrue(service.isSOSAvailable());
-        // Assert.assertTrue(service.isSOSTransactional());
     }
 
     @Test
     public void testRegisterSensor() throws Exception {
-        System.out.println(service.insertSensor(sensor));
+        String sID = service.insertSensor(sensor);
+        Assert.assertNotNull(sID);
+        Assert.assertSame(sID, sensor.getId());
+        LOGGER.debug("SensorID ist: " + sID);
     }
 
     @Test
     public void testInsertObservation() throws Exception {
-        System.out.println(service.insertObservation(sensor, foi, observation));
+        LOGGER.debug(service.insertObservation(sensor, foi, observation));
 
     }
 
     @Test
     public void testGetFeatureOfInterest() throws Exception {
-        System.out.println(service.getFeatureOfInterest(foi.getId()));
+        LOGGER.debug(service.getFeatureOfInterest(foi.getId()));
 
     }
 
     @Test
     public void testSOSGetObservation() throws Exception {
         // assertNotNull(service.getObservation());
-        System.out.println("GET-Observation-RESPONSE-Featurelist for ID "
+        LOGGER.debug("GET-Observation-RESPONSE-Featurelist for ID "
                 + sensor.getOffering().getId()
                 + ": "
-                + service.getObservation(sensor.getOffering().getId(),null,
-                        observedProperties,null));
+                + service.getObservation(sensor.getOffering().getId(), null,
+                        observedProperties, null));
     }
 
     @Test
     public void testSOSGetObservationOfSensor() throws Exception {
         // assertNotNull(service.getObservation());
-        System.out.println(service.getObservation(sensor.getOffering().getId(), sensors,
-                observedProperties,"urn:ogc:def:crs:EPSG::3857"));
+        LOGGER.debug(service.getObservation(sensor.getOffering().getId(),
+                sensors, observedProperties, "urn:ogc:def:crs:EPSG::3857"));
     }
-    // @Test
-    // public void testSOSInsertSensor() throws Exception {
-    // service.insertSensor();
-    // }
 }
